ba1858236e7203adbed8b58e39b3d552e093ec40
voody <tarasyashchuk089@gmail.com>
2024-06-12 15:34:49 +0300

    implement service

--- src/main/java/mate/academy/service/BookService.java
+++ src/main/java/mate/academy/service/BookService.java
@@ -1,7 +1,26 @@
 package mate.academy.service;
 
+import java.util.List;
+import mate.academy.model.Book;
+import mate.academy.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
