9593c749b8ec3f216cec86def0355ecc612f37de
voody <tarasyashchuk089@gmail.com>
2024-06-12 15:32:12 +0300

    init commit

---
+++ src/main/java/mate/academy/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package mate.academy.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository {
+}
---
+++ src/main/java/mate/academy/service/BookService.java
@@ -0,0 +1,7 @@
+package mate.academy.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+}
